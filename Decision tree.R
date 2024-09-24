2634d653336e0ef3e17148a0d9b1e90e0b86b384
Rahul <118988346+RahulDevvv@users.noreply.github.com>
2024-09-24 16:28:09 +0530

    Add files via upload

---
+++ Decision tree.R
@@ -0,0 +1,26 @@
+View(iris)
+str(iris)
+install.packages("rpart")
+library(rpart)
+ 
+data("iris")
+indexes=sample(150,110)
+indexes
+
+iris_train=iris[indexes,]
+iris_train
+
+iris_test=iris[indexes,]
+iris_test
+
+target=Species~Sepal.Length+Sepal.Width+Petal.Length+Petal.Width
+target
+
+tree=rpart(target,data=iris_train,method = "class")
+
+install.packages("rpart.plot")
+library("rpart.plot")
+rpart.plot(tree)
+
+predictions=predict(tree,iris_test)
+predictions
