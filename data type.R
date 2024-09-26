827a4e86f19f21c6508f6ab12e557b3ecd6b213a
Rahul <118988346+RahulDevvv@users.noreply.github.com>
2024-09-26 22:16:06 +0530

    Add files via upload

---
+++ data type.R
@@ -0,0 +1,85 @@
+# a <- b <- c <- 10
+# 
+# numeric - (10.5, 55, 787)
+# integer - (1L, 55l, 100L)
+# complex - (9 + 3i)
+# character - ("k", "R")
+# logical - (TRUE or FALSE)
+# 
+# x <- 5+10i
+# y <- 6+3i
+# z <- x+y
+# print(paste(x,y,z))
+# class(x)
+# class(y)
+# class(z)
+
+# x <- as.numeric(readline("Enter a number: "))
+# y <- as.integer(readline("Enter a integer: "))
+# z <- as.complex(readline("Enter a complex no. "))
+# print(class(x))
+# print(class(y))
+# print(class(z))
+
+# max(10,15,39)
+# min(10,15,39)
+# sqrt(16)
+# abs(-3.4) #modulas
+# ceiling(1.4) #round up
+# floor(1.4) #round down
+
+# str1 <- "abc"
+# str2 <- " abc def 
+#         ghi lmn opq
+#         rst uvw xyz"
+# print(str1)
+# print(str2)
+# cat(str1)
+# cat(str2)
+# print(nchar(str1))
+# print(nchar(str2))
+# 
+# grepl("a",str1)
+# grepl("\n",str2)
+# grepl("abc",str2)
+
+cat("abc \"hello\" abc ")
+
+# \\ Backslash
+# \n New line
+# \r Carriage return
+# \t Tab
+# \b Backspace
+
+# 4+2
+# 4-1
+# 4*3
+# 4/2
+# 5/2
+# 5 %/%2
+# 5%%2
+# 5^2
+
+# a<-10
+# 20 -> b
+# c <<- 30
+# cd <<-30
+# a <<- 20
+# 50 ->> e
+# z = 200
+
+# 100 == 100
+# 10 != 10
+# 10 > 10 
+# 10 < 20 
+# 10 <= 10
+# 10 >= 10
+
+name <- as.character(readline("Enter name: "))
+age <- as.integer(readline("Enter age: "))
+print (name)
+print(age)
+print(version)
+
+
+
