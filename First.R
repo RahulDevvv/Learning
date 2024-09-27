8fe84edbc149c3a95e0fba54b12d5525dd3c2f92
Rahul <118988346+RahulDevvv@users.noreply.github.com>
2024-09-27 08:12:33 +0530

    Add files via upload

---
+++ First.R
@@ -0,0 +1,19 @@
+a=10
+b<-20
+40->c
+d=a+b+c
+print(d)
+"Hello world"
+plot(1:10)
+for (x in 1:10) {
+  print(x)
+}
+student.name="John"
+student.roll=24
+.roll=34
+
+x <-c(1:10)
+x[(x>8) | (x<5)]
+#vector
+age = c(20,21,22,23,19)
+print(age)
