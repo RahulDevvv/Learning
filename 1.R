4f1cd11be85dda8560531aa5ebfb26e36ef38bfe
Rahul <118988346+RahulDevvv@users.noreply.github.com>
2024-09-30 00:58:25 +0530

    Add files via upload

---
+++ 1.R
@@ -0,0 +1,6 @@
+
+titanic_train <- read.csv("C:\\Users\\91709\\OneDrive\\Documents\\R Script\\test.csv")
+head(titanic_train)
+tail(titanic_train)
+summary(titanic_train)
+sapply(titanic_train,class)
