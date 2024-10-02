76209cb52cad2562987dc52ca1f78f615de8ce10
Rahul <118988346+RahulDevvv@users.noreply.github.com>
2024-10-02 08:42:41 +0530

    Add files via upload

---
+++ 2.R
@@ -0,0 +1,13 @@
+add_numbers <- function(x, y) {
+  return(x + y)
+}
+
+
+
+x <- as.numeric(readline("Enter first no. "))
+y <- as.numeric(readline("Enter Second no. "))
+print(paste("Result = ",add_numbers(x,y)))
+
+mydf <- data.frame((
+  Name = c("John, Ra")
+))
