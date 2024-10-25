a7d589fee5e0e5c4d5c4e05265bfa8404ddafe5a
Rahul <118988346+RahulDevvv@users.noreply.github.com>
2024-10-25 19:43:09 +0530

    Update 1.R

--- 1.R
+++ 1.R
@@ -3,4 +3,4 @@ titanic_train <- read.csv("C:\\Users\\91709\\OneDrive\\Documents\\R Script\\test
 head(titanic_train)
 tail(titanic_train)
 summary(titanic_train)
-sapply(titanic_train,class
+sapply(titanic_train,class)
